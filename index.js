cc609877567d4aae2a6b5cf7deefa0b8ea063fd4
RicardoVasquez1 <ricardokr@unitec.edu>
2022-11-03 18:08:43 -0600

    soy el mejor

--- index.js
+++ index.js
@@ -5,12 +5,21 @@ const app = express();
 app.use(bodyParser.json());
 
 
+const PORT = 3000;
+
 const ProductsRouter=require("./Routes/productRoute");
+const addressRouter = require("./Routes/address.router");
 
 app.use('/products', ProductsRouter);
 
 app.use('/products',ProductsRouter);
 
+app.use("/address", addressRouter);
 
-app.listen(3000);
+// app.listen(3000);
+
+app.listen(PORT, () => {
+    console.log(`app is listening to port ${PORT}`);
+  });
+  
 //comment
